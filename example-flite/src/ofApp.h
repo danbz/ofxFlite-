64ebdcce072635c3480487731d68092c66f9e509
danb <dan@buzzo.com>
2018-10-14 11:54:45 +0200

    updated poco dependency, deprecated calls, added time/date speech example

--- example-flite/src/ofApp.h
+++ example-flite/src/ofApp.h
@@ -1,93 +1,28 @@
 #pragma once
-
 #include "ofMain.h"
 
 #include "ofxFlite~.h"
 
-
+// Dan Buzzo 2018 - github.com/uwe-creative-technology/
+// for UWE Bristol, Creative Technology MSc, Creative Technology Toolkit module 2018-19
+// with modified portions from oF flite example;
+// https://github.com/s373/ofxFlite-
+// dependencies : requires poco libraries
+// ofxFlite and  ofXPoco
 
 class ofApp : public ofBaseApp{
-
-	public:
-
-		void setup(){
-
-			int sr = 44100;
-			int bs = 512;
-			float speed = 0.5f;
-			float vol = 0.7;
-
-			flite.setup(sr,bs,"Hello World.",speed,vol);
-
-			sound.listDevices();
-			sound.setDeviceID(6);
-			sound.setup(this, 2, 0, sr, bs, 4);
-			ofSetFrameRate(30);
-			ofSetWindowTitle("ofxFlite~");
-
-		}
-
-		void update(){
-
-			if(ofGetMousePressed(0)){
-				float s = (float)ofGetMouseX() / (ofGetWidth()/2);
-				flite.setSpeakSpeed(s);
-			}
-
-		}
-
-
-		// bool togglerec=false;
-		// int nvoice = 0;
-
-		void keyPressed(int key){
-			if(key=='1'){
-				flite.setText("you stupid programmer, that does not work.");
-			}
-			if(key=='2'){
-				flite.setText("1 + 1 + 10000 + 7.34 + 16, that does work");
-			}
-			if(key=='3'){
-				flite.setText("sunshine");
-			}
-			if(key=='4'){
-				flite.setText("mach speed");
-			}
-
-
-
-		}
-
-
-		void draw(){
-
-			ofSetColor(255);
-
-			ofSetColor( 100, 200, 72);
-			flite.draw(100, 400, 25);
-
-			if(flite.isThreadRunning()){
-				ofSetColor(0,255,0);
-			}else {
-				ofSetColor(255,0,0);
-			}
-			ofRect(50,10,25,25);
-
-		}
-
-		void audioOut(float * outpt, int bufferSize, int nChannels){
-			float * audiosys = flite.processBuffer();
-
-			for (int i = 0; i < bufferSize; i++){
-				 outpt[i*nChannels    ] = audiosys[i] ;
-				 outpt[i*nChannels + 1] =  audiosys[i] ;
-			 }
-
-		}
-
-
-
-		ofxFlite flite;
-		ofSoundStream	sound;
-
+    
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void audioOut(float * outpt, int bufferSize, int nChannels);
+    
+    ofxFlite flite;
+    ofSoundStream sound;
+    
 };
+   
